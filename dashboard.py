e80c7a13d2dde5973ecf70407ca87440f78bdf71
mhdaripinritonga12-alt <mhdaripinritonga12@gmail.com>
2026-02-03 10:32:57 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -100,8 +100,9 @@ if not st.session_state.login:
 
     st.image(
         "st.image(
-    "https://upload.wikimedia.org/wikipedia/commons/8/8e/Logo_Pemerintah_Provinsi_Sumatera_Utara.png",
-    width=90 ),
+     "https://upload.wikimedia.org/wikipedia/commons/8/8e/Logo_Pemerintah_Provinsi_Sumatera_Utara.png",
+        width=90
+    )
 
     st.markdown("<div class='login-title'>🔐 LOGIN DASHBOARD</div>", unsafe_allow_html=True)
 
